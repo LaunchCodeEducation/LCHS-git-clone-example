74c9d22db0cc87996062b1b2d1d961a641527159
Jim Flores <jimflores5@gmail.com>
2020-11-01 17:31:22 -0600

    Added the move_turtle and draw_reg_polygon functions.

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def turtle_setup():
     new_turtle.speed(0)
     return new_turtle
 
+def move_turtle(trtl, x_pos, y_pos):
+    trtl.penup()
+    trtl.goto(x_pos, y_pos)
+    trtl.pendown()
+
+def draw_reg_polygon(trtl, sides, length):
+    turn_angle = 360.0/sides
+    for side in range(sides):
+        trtl.forward(length)
+        trtl.left(turn_angle)
+
 def main():
     bob = turtle_setup()
     window = turtle.Screen()
